ecf70f98c88340e4d763966e7c996151c151609d
Zachary Palmer <zachbhs@uw.edu>
2020-02-07 15:24:08 -0800

    Added firebase starter

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app';
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyDeMsUdMj0mx6q4VdnxkGcdbJp5OIAL2JI",
+		  authDomain: "node-warrior.firebaseapp.com",
+		  databaseURL: "https://node-warrior.firebaseio.com",
+		  projectId: "node-warrior",
+		  storageBucket: "node-warrior.appspot.com",
+		  messagingSenderId: "1044681182278",
+		  appId: "1:1044681182278:web:5de0f9ea5433e8b7858e47",
+		  measurementId: "G-W82LT744WE"
+	};
+	
+	class Firebase {
+		constructor() {
+			app.initializeApp(firebaseConfig);
+		}
+	}
+	
+	export default Firebase;
