8e4ff261cd028d58915f725d535df30f55482dd0
Zachary Palmer <zachbhs@uw.edu>
2020-02-13 15:14:01 -0800

    Added auth and db firebase functions

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -15,6 +15,79 @@ import app from 'firebase/app';
     constructor() {
       app.initializeApp(firebaseConfig);
       this.auth = app.auth();
+      this.db = app.database();
+    }
+
+    doCreateUserWithEmailAndPassword = (email, password) => {
+        return new Promise((resolve, reject) => {
+          this.auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            if (errorCode === 'auth/weak-password') {
+              alert('The password is too weak.');
+              reject("The password is too weak.")
+            } else {
+              alert(errorMessage);
+              reject(errorMessage);
+            }
+            console.log(error);
+          }).then(() => {
+            resolve(true)
+          });
+        });
+    }
+
+    doSignInWithEmailAndPassword = (email, password) => {  
+      let errorMsg = "";   
+      return new Promise((resolve, reject) => {
+        this.auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+            errorMsg = "Wrong password";
+          } else {
+            alert(errorMessage);
+            errorMsg = errorMessage;
+          }
+          reject(errorMsg);
+          console.log(error);
+        }).then(() => {
+          resolve(true)
+        });
+      });
+    }
+
+    doSignOut = () => {
+        return this.auth.signOut();
+    }
+
+    isSignedIn = () => {
+        return this.auth.currentUser !== null;
+    }
+
+    deleteUser = () => {
+        let user = this.auth.currentUser;
+        if (user) {
+            user.delete().then(res => {
+              alert('User removed');
+            }).catch(function(error) {
+              alert('Could not delete the user');
+            });
+        }
+    }
+
+    addModule = (moduleJson) => {
+        // Get a key for a new set.
+        let newModuleKey = this.db.ref().child('modules').push().key;
+    
+        let updates = {};
+        updates['/modules/' + newModuleKey] = moduleJson;
+        // User modules is used to connect the many-to-many relationship of users to modules attempted
+        updates['/user-modules/' + moduleJson.uid + '/' + newModuleKey]
+    
+        this.db.ref().update(updates);
+        return newModuleKey;
     }
   }
   
