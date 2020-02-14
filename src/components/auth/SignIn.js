b3d4ded24dab0568ce7401a6c4ff3ee8e0b682bc
Conner Ardman <connerardman@gmail.com>
2020-02-14 15:57:48 -0800

    Routing (#9)
    * add routing for sign in / up
    
    * add auth

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>Sign In</div>
+  );
+}
+
+export default SignIn;
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>Sign Up</div>
+  );
+}
+
+export default SignUp;
