fe4c2222a795d0e53d06c63bb6d39fe81c5060cd
Conner Ardman <connerardman@gmail.com>
2020-02-14 15:48:31 -0800

    add routing for sign in / up (#8)

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import LearningModule from './LearningModule';
+import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
 import NavBar from './common/NavBar';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import '../App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* TODO going to put some form of routing here eventually */}
-      <NavBar brandTitle="Node Warrior" />
-      <LearningModule />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar brandTitle="Node Warrior" />
+          <Switch>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/">
+              {/* TODO going to put some form of routing here eventually */}
+              <LearningModule />
+            </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
