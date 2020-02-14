afdd4df4520b241df7fe432814cb3bc936901c57
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-02-14 14:41:52 -0800

    Change tabs to spaces in NavBar.js

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -6,22 +6,22 @@ import React from 'react';
  * TODO: Include other nav bar links
  */
 function NavBar(props) {
-	const { brandTitle } = props;
+  const { brandTitle } = props;
 
-	return (
-		<div className={'nav-bar-container'}>
-			<div className={'nav-back-btn'}>
-				<svg className={'hamburger-icon'}>
-					<use xlinkHref={'/website-icons.svg#hamburger-icon'}></use>
-				</svg>
-				<a href="#">Sub-Module</a>
-			</div>
-			<h1>{brandTitle}</h1>
-			<div className={'nav-links'}>
-				<a href="#">Account</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className={'nav-bar-container'}>
+      <div className={'nav-back-btn'}>
+        <svg className={'hamburger-icon'}>
+          <use xlinkHref={'/website-icons.svg#hamburger-icon'}></use>
+        </svg>
+        <a href="#">Sub-Module</a>
+      </div>
+      <h1>{brandTitle}</h1>
+      <div className={'nav-links'}>
+        <a href="#">Account</a>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
