27bbd467946e7f4bd23da14e363da464f6d681b1
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-02-23 01:34:36 -0800

    Update app routing to include the UserHomePage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import LearningModule from './LearningModule';
+import UserHomePage from './UserHomePage';
 import SignIn from './auth/SignIn';
 import SignUp from './auth/SignUp';
 import NavBar from './common/NavBar';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar brandTitle="Node Warrior" />
-          <Switch>
-            <Route path="/signin">
-              <SignIn />
-            </Route>
-            <Route path="/signup">
-              <SignUp />
-            </Route>
-            <Route path="/">
-              {/* TODO going to put some form of routing here eventually */}
-              <LearningModule />
-            </Route>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/module">
+            <LearningModule 
+              navBar={<NavBar navBarType="module" />}
+            />
+          </Route>
+          <Route path="/">
+            <UserHomePage 
+              navBar={<NavBar navBarType="homepage" />}
+            />
+          </Route>
         </Switch>
       </div>
     </Router>
