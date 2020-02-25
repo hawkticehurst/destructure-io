59390f0a1b7ebd054ea7151342213c7624f12a86
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2020-02-24 20:20:22 -0800

    Catalog & CatalogCard Components (#15)
    * npm installed and thus got a new package-lock
    
    * Update nav bar styles
    
    * Update app routing to include the UserHomePage component
    
    * Update the LearningModule component to take the NavBar as a prop
    
    * Update the NavBar component to take a nav bar type prop
    
    * Create the UserHomePage component
    
    * Rename UserHomePage to HomePage
    
    * Update nav bar styles
    
    * Dynamically render nav bar links using an array
    
    * Rename UserHomePage to HomePage
    
    * Rename UserHomePage to HomePage
    
    * Add catalog nav bar type to NavBar component
    
    * Begin implementing Catalog page
    
    * Implement catalog page, add styles, and add content
    
    * cleanup a bit
    
    Co-authored-by: Conner Ardman <connerardman@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import LearningModule from './LearningModule';
+import Catalog from './Catalog';
 import SignIn from './auth/SignIn';
 import SignUp from './auth/SignUp';
 import NavBar from './common/NavBar';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar brandTitle="Node Warrior" />
-          <Switch>
-            <Route path="/signin">
-              <SignIn />
-            </Route>
-            <Route path="/signup">
-              <SignUp />
-            </Route>
-            <Route path="/">
-              {/* TODO going to put some form of routing here eventually */}
-              <LearningModule />
-            </Route>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/module">
+            <LearningModule
+              navBar={<NavBar navBarType="module" />}
+            />
+          </Route>
+          <Route path="/catalog">
+            <Catalog
+              navBar={<NavBar navBarType="catalog" />}
+            />
+          </Route>
+          <Route path="/">
+            <div>TODO Landing Page</div>
+          </Route>
         </Switch>
       </div>
     </Router>
---
+++ src/components/Catalog.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import CatalogCard from './common/CatalogCard';
+
+/**
+ * Required Props:
+ * navBar {React Component} – Component representing the page nav bar
+ */
+function Catalog(props) {
+  const { navBar } = props;
+
+  return (
+    <div>
+      {navBar}
+      <div className="catalog-hero-container">
+        <h1>Content Catalog</h1>
+        <p>Discover a whole new way of approaching data structures.</p>
+        <hr />
+      </div>
+      <div className="catalog-card-container">
+        <CatalogCard cardTitle="Learn Nodes" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Linked Lists" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Arrays" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Stacks" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Queues" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Hash-Maps" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Trees" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Graphs" cardDescription="Learn one of the most fundamental data structures in computer science." />
+        <CatalogCard cardTitle="Learn Heaps" cardDescription="Learn one of the most fundamental data structures in computer science." />
+      </div>
+    </div>
+  );
+}
+
+export default Catalog;
