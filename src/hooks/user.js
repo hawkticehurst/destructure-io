dd11a2b51ff2798273e937e8951baef06c4bcc0b
Conner Ardman <ardmanc@cs.washington.edu>
2020-02-25 16:05:59 -0800

    add user hooks and sign out button

---
+++ src/hooks/user.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+
+export const UserContext = createContext({
+  user: null,
+});
+
+export const useFirebaseUser = () => {
+  return useContext(UserContext).user;
+};
+
+export const useAuth = () => {
+  const [state, setState] = useState(() => {
+    const user = firebase.auth().currentUser;
+    return {
+      initializing: !user,
+      user,
+    };
+  });
+
+  function onChange(user) {
+    setState({
+      initializing: false,
+      user,
+    });
+  }
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(onChange)
+    return () => unsubscribe();
+  }, [])
+
+  return state;
+}
