65ed75422d694f527c4d1b3f5635bb69df6c47b6
harik98 <harikaushik@gmail.com>
2020-05-14 11:49:57 -0700

    change about link to home

--- src/components/common/LandingNavBar.js
+++ src/components/common/LandingNavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function LandingNavBar(props) {
   const { about } = props;
 
-  let landingLink = about ? <Link to="/landing">Back to Landing</Link> : <Link to="/about">About</Link>;
+  let landingLink = about ? <Link to="/landing">Home</Link> : <Link to="/about">About</Link>;
   return (
     <div className="nav-bar-container homepage-nav-bar">
       <h1>destructure.io</h1>
