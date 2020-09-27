0d386d5695a5c4a657f99ec069a8a80832233f50
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-09-27 01:08:04 -0700

    Convert PageTing component to TS

--- src/components/common/PageTint.tsx
+++ src/components/common/PageTint.tsx
@@ -1,12 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+
+type FixMeLater = any;
 
 /**
  * Optional Props:
  * clickedTint {Function} - Called when the tint is clicked on
  * tintShown {boolean} - true if tint is shown, false otherwise
  */
-function PageTint(props) {
-  const {clickedTint, tintShown} = props;
+type Props = {
+  clickedTint: Function,
+  tintShown: boolean
+}
+
+function PageTint({ clickedTint, tintShown }: Props) {
   const [render, setRender] = useState(tintShown);
 
   useEffect(() => {
@@ -21,7 +27,7 @@ function PageTint(props) {
     }
   };
 
-  const onClick = (event) => {
+  const onClick = (event: FixMeLater) => {
     event.stopPropagation();
     if (clickedTint != null) {
       clickedTint();
@@ -30,11 +36,11 @@ function PageTint(props) {
 
   const animationClass = tintShown ? 'tint-fade-in' : 'tint-fade-out';
   return render ? (
-      <div
-        className={'page-tint ' + animationClass}
-        onAnimationEnd={onAnimationEnd}
-        onClick={onClick} />
-    ) : null
+    <div
+      className={'page-tint ' + animationClass}
+      onAnimationEnd={onAnimationEnd}
+      onClick={onClick} />
+  ) : null
 }
 
 export default PageTint;
