d7c10b4417d60cd8acebcb2547cc093743aba64d
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-09-27 01:05:50 -0700

    Convert LandingNavBar component to TS

--- src/components/common/LandingNavBar.tsx
+++ src/components/common/LandingNavBar.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function LandingNavBar(props) {
-  const { about } = props;
+type Props = {
+  about: boolean
+}
 
-  let landingLink = about ? <Link to="/landing">Home</Link> : <Link to="/about">About</Link>;
+function LandingNavBar({ about }: Props) {
+  const landingLink = about ? <Link to="/landing">Home</Link> : <Link to="/about">About</Link>;
+
   return (
     <div className="nav-bar-container homepage-nav-bar">
       <h1>destructure.io</h1>
