49e31d336db6a4ff8c819c34934d21712a669bb2
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2020-12-03 17:15:14 -0800

    Add Non-Active Project Members To New Alumni Component (#57)
    * Create Alumni component
    
    * Add Alumni component to About component
    
    * Remove non-active team members from Team component

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -1,33 +1,15 @@
 import React, { Fragment } from "react";
-import ConnerProfile from "./images/conner-profile.jpg";
 import HawkProfile from "./images/hawk-profile.jpg";
-import HariProfile from "./images/hari-profile.png";
-import ZachProfile from "./images/zach-profile.jpg";
 
 function Team() {
   return (
     <Fragment>
-      <h1>Team</h1>
+      <h1>Active Team</h1>
       <div className="team-card-container">
-        <div className="team-card">
-          <img src={ConnerProfile} alt="Conner" />
-          <h2>Conner Ardman</h2>
-          <a href="mailto:ardmanc@uw.edu">ardmanc@uw.edu</a>
-        </div>
         <div className="team-card">
           <img src={HawkProfile} alt="Hawk" />
           <h2>Hawk Ticehurst</h2>
-          <a href="mailto:hawkt88@uw.edu">hawkt88@uw.edu</a>
-        </div>
-        <div className="team-card">
-          <img src={HariProfile} alt="Hari" />
-          <h2>Hari Kaushik</h2>
-          <a href="mailto:harik98@uw.edu">harik98@uw.edu</a>
-        </div>
-        <div className="team-card">
-          <img src={ZachProfile} alt="Zach" />
-          <h2>Zach Palmer</h2>
-          <a href="mailto:zachbhs@uw.edu">zachbhs@uw.edu</a>
+          <a href="mailto:hawkticehurst@gmail.com">Email</a>
         </div>
       </div>
     </Fragment>
