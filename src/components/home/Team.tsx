68e633fc2c831aa1e7e9da25e4e7d586005a05b4
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-12-18 22:02:40 -0800

    Add all original team members back under team heading

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -1,15 +1,33 @@
 import React, { Fragment } from "react";
+import ConnerProfile from "./images/conner-profile.jpg";
 import HawkProfile from "./images/hawk-profile.jpg";
+import HariProfile from "./images/hari-profile.png";
+import ZachProfile from "./images/zach-profile.jpg";
 
 function Team() {
   return (
     <Fragment>
-      <h1>Active Team</h1>
+      <h1>Team</h1>
       <div className="team-card-container">
+        <div className="team-card">
+          <img src={ConnerProfile} alt="Conner" />
+          <h2>Conner Ardman</h2>
+          <a href="https://github.com/ConnerArdman">GitHub</a>
+        </div>
         <div className="team-card">
           <img src={HawkProfile} alt="Hawk" />
           <h2>Hawk Ticehurst</h2>
-          <a href="mailto:hawkticehurst@gmail.com">Email</a>
+          <a href="https://github.com/hawkticehurst">GitHub</a>
+        </div>
+        <div className="team-card">
+          <img src={HariProfile} alt="Hari" />
+          <h2>Hari Kaushik</h2>
+          <a href="https://github.com/harik98">GitHub</a>
+        </div>
+        <div className="team-card">
+          <img src={ZachProfile} alt="Zach" />
+          <h2>Zach Palmer</h2>
+          <a href="https://github.com/zachp98">GitHub</a>
         </div>
       </div>
     </Fragment>
