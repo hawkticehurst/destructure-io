f2fe17953b4f65ad421cf1b5893d7a257b5c71fa
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2020-12-20 18:13:23 -0800

    Setup GitHub Pages Deployment (#60)
    * Add gh-pages package and @types/highlightjs package. Also add new npm scripts for deployment
    
    * Fix type error
    
    * Add menu svg to common/images directory
    
    * Remove findDOMNode import and usage
    
    * Import and use MenuIcon svg
    
    * Fix capitalization typo of a NavBar prop
    
    * Delete website-icons.svg file

--- src/components/code-display/CodeHighlight.tsx
+++ src/components/code-display/CodeHighlight.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from "react";
-import { findDOMNode } from "react-dom";
 import hljs from "highlight.js";
 import "./highlight-styles/light.scss"; // examples of options, most need to be downloaded: https://highlightjs.org/static/demo/
 import "./highlight-styles/hidden.scss";
@@ -36,10 +35,12 @@ function CodeHighlight({
   isSelected,
   isInline,
 }: Props) {
-  const codeRef = useRef(null);
+  const codeRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    hljs.highlightBlock(findDOMNode(codeRef.current));
+    if (codeRef.current) {
+      hljs.highlightBlock(codeRef.current);
+    }
   });
 
   return (
